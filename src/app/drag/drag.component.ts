fd76171b55bbf162ba2be4e8e7b3f4e4add98a97
Adiener Lopez Velazquez <104896667+Pochyxi@users.noreply.github.com>
2024-02-24 15:13:38 +0100

    ottimizzazione

--- src/app/drag/drag.component.ts
+++ src/app/drag/drag.component.ts
@@ -1,19 +1,19 @@
 import {Component} from '@angular/core';
 import html2canvas from "html2canvas";
 
-class Ticket {
-  ticketId: string;
-  ticketName: string;
-  status: string;
+class Carta {
+  idCarta: string;
+  nomeTask: string;
+  colonnaDiAppartenenza: string;
 
-  constructor({ticketId = '', ticketName = '', status = ''}: {
-    ticketId?: string,
-    ticketName?: string,
-    status?: string
+  constructor({idCarta = '', nomeTask = '', colonnaDiAppartenenza = ''}: {
+    idCarta?: string,
+    nomeTask?: string,
+    colonnaDiAppartenenza?: string
   } = {}) {
-    this.ticketId = ticketId;
-    this.ticketName = ticketName;
-    this.status = status;
+    this.idCarta = idCarta;
+    this.nomeTask = nomeTask;
+    this.colonnaDiAppartenenza = colonnaDiAppartenenza;
   }
 }
 
@@ -23,77 +23,105 @@ class Ticket {
   styleUrls: ['./drag.component.scss']
 })
 export class DragComponent {
-  DONE = 'Done';
-  IN_PROGRESS = 'In Progress';
-  TO_DO = 'To Do';
+  // Costanti per le colonne
+  COMPLETATO = 'COMPLETATO';
+  IN_ESECUZIONE = 'IN ESECUZIONE';
+  DA_COMPLETARE = 'DA COMPLETARE';
 
-  ticketsArray: Ticket[] = [
-    {"ticketId": "adi-1", "ticketName": "Recupero password", "status": "To Do"},
-    {"ticketId": "adi-2", "ticketName": "Aggiornamento software", "status": "In Progress"},
-    {"ticketId": "adi-3", "ticketName": "Risoluzione bug grafico", "status": "Done"},
-    {"ticketId": "adi-4", "ticketName": "Ottimizzazione database", "status": "To Do"},
-    {"ticketId": "adi-5", "ticketName": "Implementazione nuova feature", "status": "In Progress"},
-    {"ticketId": "adi-6", "ticketName": "Miglioramento sicurezza sistema", "status": "Done"},
-    {"ticketId": "adi-7", "ticketName": "Refactoring codice", "status": "To Do"},
-    {"ticketId": "adi-8", "ticketName": "Integrazione API esterne", "status": "In Progress"},
-    {"ticketId": "adi-9", "ticketName": "Test prestazionali", "status": "Done"},
-    {"ticketId": "adi-10", "ticketName": "Rinnovo certificati SSL", "status": "To Do"}
+  // Mockup di dati
+  arrayDiCarte: Carta[] = [
+    {"idCarta": "adi-1", "nomeTask": "Recupero password", "colonnaDiAppartenenza": "DA COMPLETARE"},
+    {"idCarta": "adi-2", "nomeTask": "Aggiornamento software", "colonnaDiAppartenenza": "IN ESECUZIONE"},
+    {"idCarta": "adi-3", "nomeTask": "Risoluzione bug grafico", "colonnaDiAppartenenza": "COMPLETATO"},
+    {"idCarta": "adi-4", "nomeTask": "Ottimizzazione database", "colonnaDiAppartenenza": "DA COMPLETARE"},
+    {"idCarta": "adi-5", "nomeTask": "Implementazione nuova feature", "colonnaDiAppartenenza": "IN ESECUZIONE"},
+    {"idCarta": "adi-6", "nomeTask": "Miglioramento sicurezza sistema", "colonnaDiAppartenenza": "COMPLETATO"},
+    {"idCarta": "adi-7", "nomeTask": "Refactoring codice", "colonnaDiAppartenenza": "DA COMPLETARE"},
+    {"idCarta": "adi-8", "nomeTask": "Integrazione API esterne", "colonnaDiAppartenenza": "IN ESECUZIONE"},
+    {"idCarta": "adi-9", "nomeTask": "Test prestazionali", "colonnaDiAppartenenza": "COMPLETATO"},
+    {"idCarta": "adi-10", "nomeTask": "Rinnovo certificati SSL", "colonnaDiAppartenenza": "DA COMPLETARE"},
   ];
-  currentItem = new Ticket();
 
-  overColumn = '';
+  // Card corrente trascinata
+  cartaTrascinata = new Carta();
 
-  filterTickets(status: string) {
-    return this.ticketsArray.filter(ticket => ticket.status === status);
-  }
+  // Colonna su cui si sta trascinando la carta
+  colonnaDragOver = '';
 
   // Inizio trascinamento
-  onDragStart(item: Ticket) {
-    console.log('onDragStart', item);
-    this.currentItem = item;
-  }
-
-  // Fine trascinamento
-  onDrop(event: DragEvent, status: string) {
-    console.log('onDrop', event);
-    console.log(this.currentItem);
-
-    const ticket = this.ticketsArray.find(ticket => ticket.ticketId === this.currentItem.ticketId);
-
-    if (ticket) {
-      ticket.status = status;
-    }
-
-    this.currentItem = new Ticket();
-    // console.log('Verifica della presenza del ticket', ticket);
-    // console.log(this.filterTickets(this.TO_DO));
-    //
-    // console.log('Verifica del resetto del currentItem');
-    // console.log(this.currentItem);
-
-    this.overColumn = '';
-    event.target && (event.target as HTMLElement).classList.remove('card-drag-over');
+  onDragStart(item: Carta) {
+    // Imposto l'elemento corrente trascinato
+    this.cartaTrascinata = item;
   }
 
   // Trascinamento al passaggio sopra
-  onDragOver(event: DragEvent, sectionName: string) {
-    if(this.overColumn !== sectionName) {
-      this.overColumn = sectionName;
+  onDragOver(evento: DragEvent, nomeSezione: string) {
+    evento.preventDefault();
 
-      if (this.currentItem.status === sectionName) {
-        return;
+    // Evita di eseguire il codice se la colonna è già evidenziata
+    if (this.colonnaDragOver !== nomeSezione) {
+      this.colonnaDragOver = nomeSezione;
+
+      // Se la carta è nella colonna di partenza, non fare nulla
+      if (this.cartaTrascinata.colonnaDiAppartenenza !== nomeSezione) {
+
+        // Aggiungo la classe di passaggio
+        this.manipolazioneClassePassaggio(evento, true);
+      } else {
+        this.colonnaDragOver = '';
       }
-
-      const target = event.target as HTMLElement;
-      target.classList.add('card-drag-over');
     }
-
-    event.preventDefault();
-    event.stopPropagation();
   }
 
-  onDragLeave(event: DragEvent) {
-    this.overColumn = '';
-    event.target && (event.target as HTMLElement).classList.remove('card-drag-over');
+  // Trascinamento al passaggio fuori
+  onDragLeave(evento: DragEvent) {
+    evento.preventDefault();
+
+    // Resetto la colonna evidenziata
+    this.colonnaDragOver = '';
+
+    // Rimuovo la classe di passaggio
+    this.manipolazioneClassePassaggio(evento);
+  }
+
+  // Fine trascinamento
+  onDrop(evento: DragEvent, colonnaDiAppartenenza: string) {
+    evento.preventDefault();
+
+    // Recupero la carta corrispondente
+    const carta = this.arrayDiCarte.find(carta => carta.idCarta === this.cartaTrascinata.idCarta);
+
+    // Aggiorno lo stato della carta
+    if (carta) {
+      carta.colonnaDiAppartenenza = colonnaDiAppartenenza;
+    }
+
+    // Resetto la carta corrente e la colonna evidenziata
+    this.cartaTrascinata = new Carta();
+    this.colonnaDragOver = '';
+
+    // Rimuovo la classe di passaggio
+    this.manipolazioneClassePassaggio(evento);
+  }
+
+  // Filtra tutte le carte per stato
+  filtraCartePerColonna(stato: string) {
+    return this.arrayDiCarte.filter(carta => carta.colonnaDiAppartenenza === stato);
+  }
+
+  manipolazioneClassePassaggio(evento: DragEvent, add?: boolean) {
+    // recupero l'elemento che ha scatenato l'evento
+    let colonnaNellHTML = evento.target as HTMLElement;
+
+    // Questo approccio evita che anche le carta figlie vengano evidenziate
+    const colonnaPiuVicina = colonnaNellHTML.closest('.colonna_kanban');
+    if (colonnaPiuVicina) {
+      if (add) {
+        colonnaPiuVicina.classList.add('la_carta_sta_passando');
+      } else {
+        colonnaPiuVicina.classList.remove('la_carta_sta_passando');
+      }
+    }
+
   }
 }
