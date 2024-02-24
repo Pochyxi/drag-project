158e69f890408602fb96cefc9c0ec44e34bddc8b
Adiener Lopez Velazquez <104896667+Pochyxi@users.noreply.github.com>
2024-02-24 01:47:16 +0100

    first version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DragComponent } from './drag/drag.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DragComponent
   ],
   imports: [
     BrowserModule,
